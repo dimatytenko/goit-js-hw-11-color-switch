01ca6624abd2f1b57c1bc0585030f5070d8d90cd
Dima Tytenko <amgpetronass@gmail.com>
2021-10-05 21:23:28 +0300

    finished

---
+++ index.js
@@ -0,0 +1,40 @@
+const buttonElementStart = document.querySelector('[data-action="start"]');
+const buttonElementEnd = document.querySelector('[data-action="stop"]');
+const bodyElement = document.querySelector('body');
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+buttonElementStart.addEventListener('click', onClickStart);
+buttonElementEnd.addEventListener('click', onClickStop);
+
+let intervalId = null;
+let isActive = false;
+
+function onClickStart() {
+  if (isActive) {
+    return;
+  }
+
+  isActive = true;
+  intervalId = setInterval(() => {
+    const currentColor =
+      colors[randomIntegerFromInterval(0, colors.length - 1)];
+    console.log(currentColor);
+    bodyElement.style.backgroundColor = currentColor;
+  }, 1000);
+}
+function onClickStop() {
+  clearInterval(intervalId);
+  isActive = false;
+}
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
